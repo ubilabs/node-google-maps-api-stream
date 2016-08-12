20808e063b0fc77a0cff740cdaedbf49c03da914
Keno Schwalb <keno@kenoschwalb.com>
2016-08-12 11:49:40 +0200

    feat(examples): add an example for Static Maps Images

---
+++ examples/static-maps-image.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import fs from 'fs';
+import {StaticMapsImage} from '../src/index';
+
+const staticMaps = new StaticMapsImage({
+  googleMaps: {
+    key: 'your-key'
+  },
+  queriesPerSecond: 5,
+  cacheFile: null // flat-file-db really wasn't built for caching images
+});
+
+staticMaps.on('data', data => {
+  if(data.error) {
+    console.error('ERROR:', data.error);
+  }
+
+  fs.writeFileSync(`${data.stats.current}.jpg`, data.response, 'binary');
+});
+staticMaps.on('end', () => console.log('end'));
+
+staticMaps.write({
+  center: '53.5610771,9.9569145',
+  format: 'jpg',
+  zoom: 15,
+  size: '640x640'
+});
+staticMaps.end();
