4b50649dcdaab72363773911d7a1eacd9bac94d1
Keno Schwalb <keno@kenoschwalb.com>
2016-08-12 12:05:30 +0200

    feat(endpoints): add reverse geocoding endpoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export {Directions} from './endpoints/directions';
 export {Geocoding} from './endpoints/geocoding';
+export {ReverseGeocoding} from './endpoints/reverse-geocoding';
 export {StaticMapsImage} from './endpoints/static-maps-image';
 export {StaticMapsURL} from './endpoints/static-maps-url';
